6e2f1a08c56ca728f0e6718545c0d9f704bad739
M1lksy <marcschreuder1@gmail.com>
2025-04-13 11:11:28 +1000

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import os
+import subprocess
+import sys
+
+# Ensure yfinance is installed
+try:
+    import yfinance
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "yfinance"])
+    import yfinance
 import streamlit as st
 import pandas as pd
 import yfinance as yf
